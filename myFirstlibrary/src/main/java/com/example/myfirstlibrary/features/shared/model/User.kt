25763df337b6b837a72632e7f9f1e3e5705c51e6
Sudeep Bajracharya <sudeep.bajracharya@ekbana.info>
2022-09-13 13:07:40 +0545

    minor change

--- myFirstlibrary/src/main/java/com/example/myfirstlibrary/features/shared/model/User.kt
+++ myFirstlibrary/src/main/java/com/example/myfirstlibrary/features/shared/model/User.kt
@@ -1,8 +1,16 @@
 package com.example.myfirstlibrary.features.shared.model
 
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+@Keep
 data class User(
+    @SerializedName("id")
     var id: String? = null,
+    @SerializedName("name")
     var name: String? = null,
+    @SerializedName("avatar")
     var avatar: String? = null,
+    @SerializedName("email")
     var email: String? = null
 )
